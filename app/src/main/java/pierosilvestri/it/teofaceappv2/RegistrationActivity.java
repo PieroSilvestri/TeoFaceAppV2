13934b0b70a7867ba7dd665005738973b5a56e7b
munsra <piero.silvestri93@gmail.com>
2017-06-07 12:16:07 +0200

    Added Image Taker
    Bisogna cambiare il metodo di controllo della imageView.

--- app/src/main/java/pierosilvestri/it/teofaceappv2/RegistrationActivity.java
+++ app/src/main/java/pierosilvestri/it/teofaceappv2/RegistrationActivity.java
@@ -1,5 +1,11 @@
 package pierosilvestri.it.teofaceappv2;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,11 +15,21 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class RegistrationActivity extends AppCompatActivity {
 
     private static final String TAG = "REGISTRATIONACTIVITY";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
     private Button btnDone;
+
+    private int value;
     private ImageView img1, img2;
+    String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,10 +84,39 @@ public class RegistrationActivity extends AppCompatActivity {
 
     void Img1_Clicked(){
         Log.d(TAG, "Img1_Clicked");
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            value = 1;
+            // TODO cercare un metodo migliore
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+        // TODO CROP IMAGE
     }
 
     void Img2_Clicked(){
         Log.d(TAG, "Img2_Clicked");
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            value = 2;
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            if(value == 1){
+                img1.setImageBitmap(imageBitmap);
+            }else if(value == 2){
+                img2.setImageBitmap(imageBitmap);
+            }else{
+                Log.d(TAG, "ERROR. VALUE NOT FOUND.");
+            }
+        }
+    }
+
+
 
 }
